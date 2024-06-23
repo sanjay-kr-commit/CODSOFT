8ba819db54d968cf68c20c58ab6b1432dba0e713
sanjay-kr-commit <sanjay.kr.on@gmail.com>
2024-06-23 09:01:08 +0530

    Supply Correct Table Name

--- TodoListCODSOFT/app/src/main/java/codsoft/internship/sanjay/todolist/TodoListDB.kt
+++ TodoListCODSOFT/app/src/main/java/codsoft/internship/sanjay/todolist/TodoListDB.kt
@@ -18,21 +18,22 @@ class TodoListDB(
     DATABASE_VERSION
 ) {
 
-    override fun onCreate(db: SQLiteDatabase) {
-        db.execSQL( TABLE )
+    override fun onCreate(db: SQLiteDatabase?) {
+        db?.execSQL( TABLE )
     }
 
     @SuppressLint("Recycle", "Range")
-    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
         databaseUpdateStatus.value = "Updating From version $oldVersion to $newVersion"
+        db?.execSQL( TABLE )
         val alteredTableName = "ALTERED_TABLE"
         try {
             when (newVersion) {
                 2 -> {
                     databaseUpdateStatus.value = "Creating Temporary Table"
-                    db.execSQL(alteredTableName.AlteredTABLE)
+                    db?.execSQL(alteredTableName.AlteredTABLE)
                     databaseUpdateStatus.value = "Migrate Data To New Table"
-                    db.rawQuery("SELECT * FROM ${TodoEntry.TABLE}", arrayOf()).use { cursor ->
+                    db?.rawQuery("SELECT * FROM ${TodoEntry.TABLE}", arrayOf())?.use { cursor ->
                         val columns = cursor.columnNames
                         while (cursor.moveToNext()) {
                             ContentValues().apply {
@@ -48,17 +49,17 @@ class TodoListDB(
                         }
                     }
                     databaseUpdateStatus.value = "Drop Old Table"
-                    db.execSQL( "DROP TABLE IF EXISTS ${TodoEntry.TABLE}" )
+                    db?.execSQL( "DROP TABLE IF EXISTS ${TodoEntry.TABLE}" )
                     databaseUpdateStatus.value = "Replace Table"
-                    db.execSQL( "ALTER TABLE $alteredTableName RENAME TO ${TodoEntry.TABLE}" )
+                    db?.execSQL( "ALTER TABLE $alteredTableName RENAME TO ${TodoEntry.TABLE}" )
                     databaseUpdateStatus.value = "Drop Temporary Table"
-                    db.execSQL( "DROP TABLE IF EXISTS $alteredTableName" )
+                    db?.execSQL( "DROP TABLE IF EXISTS $alteredTableName" )
                 }
             }
         } catch ( _ : Exception ) {
             databaseUpdateStatus.value = "FAILED TO UPDATE, DROPPING TABLE"
-            db.execSQL( "DROP TABLE IF EXISTS $alteredTableName" )
-            db.execSQL( "DROP TABLE IF EXISTS ${TodoEntry.TABLE}" )
+            db?.execSQL( "DROP TABLE IF EXISTS $alteredTableName" )
+            db?.execSQL( "DROP TABLE IF EXISTS ${TodoEntry.TABLE}" )
             onCreate( db )
         }
         databaseUpdateStatus.value = null
@@ -68,7 +69,7 @@ class TodoListDB(
         private const val DATABASE_NAME : String = "TODO_LIST.db"
         private const val DATABASE_VERSION : Int = 2
         private val TABLE : String
-            get() = TodoEntry.TITLE.toString().AlteredTABLE
+            get() = TodoEntry.TABLE.toString().AlteredTABLE
         private val String.AlteredTABLE
             get() = """
             CREATE TABLE IF NOT EXISTS ${this}(
